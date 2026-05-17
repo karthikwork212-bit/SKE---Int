679ebbd49e9916bbb6c27af7b19d16780f1ce6e9
Karthik <karthikwork212@gmail.com>
2026-05-17 14:05:42 +0530

    fixed app listen

--- backend/server.js
+++ backend/server.js
@@ -51,3 +51,8 @@ app.post("/api/contact", async (req, res) => {
     });
   }
 });
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
